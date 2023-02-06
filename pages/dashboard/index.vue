f07c3a3bdf431ba2439e57539ccbf182a0adea50
CrisSot0 <612110440@utsalamanca.edu.mx>
2023-02-06 13:44:02 -0600

    No jalaba pq no tenia el .env :c

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>hola</h1>
+  <div style="padding: 20px;">
+    <principal/>
+  </div>
 </template>
 
 <script>
+import principal from '../../components/dashboard/principal.vue';
 export default {
   auth: true,
-  layout: 'dashboard'
+  layout: 'dashboard',
+  components: {
+    principal
+  }
 }
 </script>
   
